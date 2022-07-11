ad2e8718803e93be3afa24db6daadeb61d707eef
Sergei Solodzhuk <solodzh@gmail.com>
2022-07-12 00:58:58 +0200

    верстка

---
+++ frontend/src/components/TagsBlock.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from "react";
+
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import TagIcon from "@mui/icons-material/Tag";
+import ListItemText from "@mui/material/ListItemText";
+import Skeleton from "@mui/material/Skeleton";
+
+import { SideBlock } from "./SideBlock";
+
+type TagsBlockPropsType = {
+  items: string[];
+  isLoading: boolean;
+}
+
+export const TagsBlock: FC<TagsBlockPropsType> = ({ items, isLoading = true }) => {
+  return (
+    <SideBlock title="Тэги">
+      <List>
+        {(isLoading ? [...Array(5)] : items).map((name, i) => (
+          <a
+            style={{ textDecoration: "none", color: "black" }}
+            href={`/tags/${name}`}
+          >
+            <ListItem key={i} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TagIcon />
+                </ListItemIcon>
+                {isLoading ? (
+                  <Skeleton width={100} />
+                ) : (
+                  <ListItemText primary={name} />
+                )}
+              </ListItemButton>
+            </ListItem>
+          </a>
+        ))}
+      </List>
+    </SideBlock>
+  );
+};
---
+++ frontend/src/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import "./index.scss";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <React.StrictMode>
+            <CssBaseline />
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </React.StrictMode>
+    );
+
+}
