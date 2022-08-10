c4b9384ef6b7f1a2e7613c8033d7b7681a96e2f7
Сергей Солоджук <82179236+solodzh78@users.noreply.github.com>
2022-08-10 10:20:02 +0200

    Delete constants.js

--- constants.js
+++
@@ -1,2 +0,0 @@
-export const MONGO_DB_URI = process.env.MONGO_DB_URI || 'mongodb+srv://admin:wwwwww@cluster0.g2etn.mongodb.net/blog?retryWrites=true&w=majority';
-export const PORT = process.env.PORT || 4444;
