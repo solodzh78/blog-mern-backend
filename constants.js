92560a1ab8bd0b72ae544d0bc85cb71a163e53d3
Sergei Solodzhuk <solodzh@gmail.com>
2022-08-10 10:18:29 +0200

    исправил путь к файлу превью

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-export const MONGO_DB_URI = process.env.MONGO_DB_URI || 'mongodb+srv://admin:wwwwww@cluster0.g2etn.mongodb.net/blog1?retryWrites=true&w=majority';
+export const MONGO_DB_URI = process.env.MONGO_DB_URI || 'mongodb+srv://admin:wwwwww@cluster0.g2etn.mongodb.net/blog?retryWrites=true&w=majority';
 export const PORT = process.env.PORT || 4444;
